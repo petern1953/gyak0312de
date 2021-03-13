ef6918dbc9ef7a6fa625b3b4126584e5d1a31cda
petern1953 <petern1953@gmail.com>
2021-03-13 14:57:33 +0100

    app routing, app component, userlist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserlistComponent } from './page/userlist/userlist.component';
+import { AdminComponent } from './page/admin/admin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {
+    path: '',
+    component: UserlistComponent
+  },
+  {
+    path: 'user/:id',
+    component: AdminComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
