b9c00b6efa67be0b6abc210fdedadc4f26382000
petern1953 <petern1953@gmail.com>
2021-03-13 16:14:28 +0100

    user list column select, scss

--- src/app/page/userlist/userlist.component.ts
+++ src/app/page/userlist/userlist.component.ts
@@ -17,6 +17,10 @@ export class UserlistComponent implements OnInit {
   phraseControl: FormControl = new FormControl('');
   phrase: string = '';
 
+  order: number = 1;
+  columnKey: string = '';
+  searchText: string = 'Search for / filter | sort by';
+
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
@@ -28,5 +32,21 @@ export class UserlistComponent implements OnInit {
 
   onDelete(id: number): void {
     this.userService.remove(id);
+  }
+
+  changeOrder(): void {
+    console.log(this.order);
+    this.order = -this.order;
+  }
+
+  onColumnSelect(key: string): void {
+    this.columnKey = key;
+    this.changeOrder();
+    this.searchText = 'Search for / filter | sort by ' + `${this.columnKey}`;
+    // this.phrase = '';
+  }
+
+  onChangePhrase(event: Event): void {
+    this.phrase = (event.target as HTMLInputElement).value;
   }
 }
